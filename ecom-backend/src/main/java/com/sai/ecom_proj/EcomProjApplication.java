6ed540708018440865b7281a48ce41814be6ca5e
Sai Jaswanth Potagani <potaganijaswanth203@gmail.com>
2025-09-26 16:05:55 +0530

    Added backend project

--- ecom-backend/src/main/java/com/sai/ecom_proj/EcomProjApplication.java
+++ ecom-backend/src/main/java/com/sai/ecom_proj/EcomProjApplication.java
@@ -1,4 +1,4 @@
-package com.sai.ecom_proj;
+package src.main.java.com.sai.ecom_proj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
